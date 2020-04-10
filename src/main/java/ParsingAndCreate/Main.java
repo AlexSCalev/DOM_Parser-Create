b2ebe39d3612dbdf681dc60c78dbd9085f9b5e4f
Alexandr_Calev <aleks.calev@mail.ru>
2020-04-10 21:54:27 +0300

    final version

---
+++ src/main/java/ParsingAndCreate/Main.java
@@ -0,0 +1,14 @@
+package ParsingAndCreate;
+
+public class Main {
+    public static void main(String[] args) {
+//        Show Dom Parsing Result
+        Dom_Parsing parser = new Dom_Parsing();
+        parser.parser("C:\\Users\\Alexandr\\IdeaProjects\\AditionalParsingAndCreate\\src\\main\\java\\ParsingAndCreate\\Aditional.txt");
+//        Create two File if indicate awayToResorces and awayFile0 and awayFile1
+//        Dom_CreateXML objectCreate = new Dom_CreateXML();
+//        objectCreate.inputFile("C:\\Users\\Alexandr\\Desktop\\Generate File\\Aditional.txt",
+//                "C:\\Users\\Alexandr\\Desktop\\Generate File\\Aditional1.txt",
+//                "C:\\Users\\Alexandr\\Desktop\\Generate File\\Aditional2.txt");
+    }
+}
